139993b8ddee252724df3dd35dc50b9763024754
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-04-29 19:18:47 +0200

    libft : added function ft_reset_ptr, cleaned makefile, put all include in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,20 +6,25 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 13:38:12 by tmongell          #+#    #+#             */
-/*   Updated: 2021/11/18 18:05:10 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/04/29 19:14:04 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 //structs :
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
 // function from part 1 :
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -44,6 +49,7 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 //function from part 2 :
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -56,12 +62,15 @@ void	ft_putstr_fd(char *c, int fd);
 void	ft_putendl_fd(char *c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 //functions from bonus
+
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **alst, t_list *new);
 int		ft_lstsize(t_list	*lst);
 t_list	*ft_lstlast(t_list	*lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-//additional functions (not mandatory, but still used)
+//additional functions
+
 char	*ft_strndup(const char *s1, size_t n);
+void	*ft_reset_ptr(void	*ptr);
 #endif
