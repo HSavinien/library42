5c1294a0334d6034afada3d4f03fe681132148f1
Theo Mongellaz <theo.mongellaz@gmail.com>
2023-02-24 02:00:07 +0100

    added class template. added better Makefile

---
+++ template/Class.cpp
@@ -0,0 +1,37 @@
+#include "Class.hpp"
+
+/*public----------------------------------------------------------------public*/
+/*constructors/destructors*/
+Class::Class(void) {
+	return;
+}
+Class::Class(const Class& src) {
+	*this = src;
+}
+Class::~Class(void) {
+	return;
+}
+
+/*operators overload*/
+Class&	Class::operator=(const Class& src) {
+	//copy member atribute of src
+}
+
+/*accessors*/
+
+/*member functions*/
+
+/*non-member functions*/
+
+/*protected----------------------------------------------------------protected*/
+/*accessors*/
+/*member functions*/
+
+/*non-member functions*/
+
+/*private--------------------------------------------------------------private*/
+/*member functions*/
+
+/*non-member functions*/
+
+/*externals----------------------------------------------------------externals*/
---
+++ template/Class.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+
+class	Class {
+	public:
+	/*constructors/destructors*/
+	Class(void);
+	Class(const Class& src);
+	~Class(void);
+
+	/*operator overload*/
+	Class&	operator=(const Class& src);
+
+	/*accessors*/
+
+	/*members functions*/
+
+	/*non-member functions*/
+
+	protected:
+
+	/*accessors*/
+
+	/*members functions*/
+
+	/*non-member functions*/
+
+
+	private:
+
+	/*member attribute*/
+
+	/*non-member attribute*/
+
+	/*member functions*/
+
+	/*non-member functions*/
+
+};
